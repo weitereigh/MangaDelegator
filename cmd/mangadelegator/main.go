8177685013c88cdbbc33e02b2d292aea75d3f3f7
Weit <caroxmeme@gmail.com>
2025-10-27 00:18:26 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/mangadelegator/main.go
@@ -0,0 +1,23 @@
+// cmd/mangadelegator/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "mangadelegator/internal/mangadelegator"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := mangadelegator.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
